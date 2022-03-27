09b22698af904ca97e153cb10363095e774144ae
Ardee Aram <ardee.aram@safelinkhub.com>
2022-03-27 16:40:52 +0000

    Able to use AWS SDK

--- app.js
+++ app.js
@@ -3,11 +3,29 @@ import "@babel/polyfill";
 import "cross-fetch/polyfill";
 import Koa from 'koa';
 import serverless from 'serverless-http';
+import { EC2 } from '@aws-sdk/client-ec2';
 
 const app = new Koa();
 
 // response
 app.use(async ctx => {
+  const ec2 = new EC2({region: 'us-east-1', maxRetries: 15});
+   var params = {
+    InstanceIds: [
+       "i-055e44a095b21d5bf"
+    ]
+   };
+  const result = await ec2.describeInstances(params);
+  /*
+   ec2.describeInstances(params, function(err, data) {
+     if (err) console.log(err, err.stack); // an error occurred
+     else     console.log(data);           // successful response
+     /*
+     data = {
+     }
+   });
+     */
+  console.log(result.Reservations.map(x => x.Instances[0].State.Name));
   const slackUrl = process.env.SLACK_URL;
   console.log(`SLACKURL: ${slackUrl}`);
   /*global fetch*/
